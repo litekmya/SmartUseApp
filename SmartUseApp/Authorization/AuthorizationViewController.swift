8672b3a079ca74fef23d394be6977ef375947355
Vladimir Li <tekmya@mail.ru>
2022-05-18 22:14:59 +0300

    Удалил storyboard Добавил фреймворки: SnapKit, Firebase Начал сборку под MVVM

---
+++ SmartUseApp/Authorization/AuthorizationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationViewController.swift
+//  SmartUseApp
+//
+//  Created by Владимир Ли on 18.05.2022.
+//
+
+import UIKit
+import SnapKit
+
+class AuthorizationViewController: UIViewController {
+    
+    private var imageView: UIImageView!
+    private var authLabel: UILabel!
+    
+    private var loginTextField: UITextField!
+    private var passwordTextField: UITextField!
+    
+    private var loginButton: UIButton!
+    private var registrationButton: UIButton!
+    private var signInWithAppleButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ SmartUseApp/Authorization/AuthorizationViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AuthorizationViewModel.swift
+//  SmartUseApp
+//
+//  Created by Владимир Ли on 18.05.2022.
+//
+
+import Foundation
+
+protocol AuthorizationViewModelProtocol {
+    
+}
+
+class AuthorizationViewModel: AuthorizationViewModelProtocol {
+    
+}
